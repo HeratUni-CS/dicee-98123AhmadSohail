538037963d91404fa86228531d352e5bad101836
Sohail <ahmadsohailmaghsoudi@gmail.com>
2023-05-05 12:52:17 +0430

    a stateless widget with no functionality to show on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
+  return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          title: Text('Dicee'),
           backgroundColor: Colors.red,
-          title: Text('Dice'),
         ),
-        body: MyApp(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class DicePage extends StatelessWidget {
+  var leftDiceNumber = 4;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('left dice got pressed.');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('right button has got pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
